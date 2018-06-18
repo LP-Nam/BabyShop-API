16fb29896a0980655a4be7d61ebc193bc6188800
LuanPham <luanpham.997@gmail.com>
2018-06-18 19:24:26 +0700

    push host

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -3,24 +3,24 @@ var mysql = require('mysql');
 
 // Initialize pool for local
 
-var pool      =    mysql.createPool({
-    connectionLimit : 1000,
-    host     : '127.0.0.1',
-    user     : 'root',
-	password : '',
-    database : 'bansach',
-    debug    :  false
-});
+// var pool      =    mysql.createPool({
+//     connectionLimit : 1000,
+//     host     : '127.0.0.1',
+//     user     : 'root',
+// 	password : '',
+//     database : 'bansach',
+//     debug    :  false
+// });
 
 // Initialize pool for heroku host
 
-// var pool      =    mysql.createPool({
-//     connectionLimit : 50,
-//     host     : 'db4free.net',
-//     user     : 'luanpham2',
-//     password : 'luan1997.',
-//     database : 'bookstore_db',
-//     debug    :  false
-// });
+var pool      =    mysql.createPool({
+    connectionLimit : 550,
+    host     : 'db4free.net',
+    user     : 'luanpham2',
+    password : 'luan1997.',
+    database : 'bookstore_db',
+    debug    :  false
+});
 
 module.exports = pool;
