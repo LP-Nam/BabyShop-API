4f378b5d0341da1cc7de1f4265a0ba90a75588f7
LuanPham <luanpham.997@gmail.com>
2018-06-19 10:17:25 +0700

    change host

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -13,13 +13,23 @@ var mysql = require('mysql');
 // });
 
 // Initialize pool for heroku host
+// var pool      =    mysql.createPool({
+//     connectionLimit : 1000,
+//     host     : 'db4free.net',
+//     user     : 'luanpham2',
+//     password : 'luan1997.',
+//     database : 'bookstore_db',
+//     debug    :  false
+// });
 
+
+// Initialize pool for freemysql host
 var pool      =    mysql.createPool({
     connectionLimit : 1000,
-    host     : 'db4free.net',
-    user     : 'luanpham2',
-    password : 'luan1997.',
-    database : 'bookstore_db',
+    host     : 'sql12.freemysqlhosting.net',
+    user     : 'sql12243562',
+    password : 'SmVukUbc9u',
+    database : 'sql12243562',
     debug    :  false
 });
 
