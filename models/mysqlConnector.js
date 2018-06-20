d49c082112812e86575e13916bcfe946592c99c3
LuanPham <luanpham.997@gmail.com>
2018-06-20 10:54:13 +0700

    test host

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -3,14 +3,14 @@ var mysql = require('mysql');
 
 // Initialize pool for local
 
-// var pool      =    mysql.createPool({
-//     connectionLimit : 1000,
-//     host     : '127.0.0.1',
-//     user     : 'root',
-// 	password : '',
-//     database : 'bansach',
-//     debug    :  false
-// });
+var pool      =    mysql.createPool({
+    connectionLimit : 1000,
+    host     : '127.0.0.1',
+    user     : 'root',
+	password : '',
+    database : 'bansach',
+    debug    :  false
+});
 
 // Initialize pool for heroku host
 // var pool      =    mysql.createPool({
@@ -24,13 +24,13 @@ var mysql = require('mysql');
 
 
 // Initialize pool for freemysql host
-var pool      =    mysql.createPool({
-    connectionLimit : 1000,
-    host     : 'sql12.freemysqlhosting.net',
-    user     : 'sql12243562',
-    password : 'SmVukUbc9u',
-    database : 'sql12243562',
-    debug    :  false
-});
+// var pool      =    mysql.createPool({
+//     connectionLimit : 1000,
+//     host     : 'sql12.freemysqlhosting.net',
+//     user     : 'sql12243562',
+//     password : 'SmVukUbc9u',
+//     database : 'sql12243562',
+//     debug    :  false
+// });
 
 module.exports = pool;
